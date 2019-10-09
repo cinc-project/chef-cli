31c564f44e0d41e97671abac7a6ee7e537edd716
Lance Albertson <lance@osuosl.org>
2019-10-08 22:32:34 -0700

    Cinc Stable patches

--- lib/chef-cli/dist.rb
+++ lib/chef-cli/dist.rb
@@ -3,47 +3,47 @@ module ChefCLI
     # This class is not fully implemented, depending on it is not recommended!
 
     # The full marketing name of the product
-    PRODUCT = "Chef Workstation".freeze
-    PRODUCT_PKG_HOME = "chef-workstation".freeze
+    PRODUCT = "Cinc Workstation".freeze
+    PRODUCT_PKG_HOME = "cinc-workstation".freeze
 
     # the name of the chef-cli gem
-    CLI_PRODUCT = "Chef CLI".freeze
+    CLI_PRODUCT = "Cinc CLI".freeze
     CLI_GEM = "chef-cli".freeze
 
     # the name of the overall infra product
-    INFRA_PRODUCT = "Chef Infra".freeze
+    INFRA_PRODUCT = "Cinc Infra".freeze
 
     # name of the Infra client product
-    INFRA_CLIENT_PRODUCT = "Chef Infra Client".freeze
+    INFRA_CLIENT_PRODUCT = "Cinc Client".freeze
 
     # The client's alias (chef-client)
-    INFRA_CLIENT_CLI = "chef-client".freeze
+    INFRA_CLIENT_CLI = "cinc-client".freeze
 
     # The client's gem
     INFRA_CLIENT_GEM = "chef".freeze
 
-    INSPEC_PRODUCT = "Chef InSpec".freeze
-    INSPEC_CLI = "inspec".freeze
+    INSPEC_PRODUCT = "Cinc Auditor".freeze
+    INSPEC_CLI = "cinc-auditor-bin".freeze
 
     # The name of the server product
-    SERVER_PRODUCT = "Chef Infra Server".freeze
-    WORKFLOW = "Chef Workflow (Delivery)".freeze
+    SERVER_PRODUCT = "Cinc Infra Server".freeze
+    WORKFLOW = "Cinc Workflow (Delivery)".freeze
 
     # The chef executable, as in `chef gem install` or `chef generate cookbook`
-    EXEC = "chef".freeze
+    EXEC = "cinc".freeze
 
     # Chef-Zero's product name
-    ZERO_PRODUCT = "Chef Infra Zero".freeze
+    ZERO_PRODUCT = "Cinc Infra Zero".freeze
 
-    HAB_PRODUCT = "Chef Habitat".freeze
-    HAB_SOFTWARE_NAME = "habitat".freeze
-    HAB_CLI = "hab".freeze
+    HAB_PRODUCT = "Biome".freeze
+    HAB_SOFTWARE_NAME = "biome".freeze
+    HAB_CLI = "bio".freeze
 
     # product patents page
     PATENTS = "https://www.chef.io/patents".freeze
 
     # Workstation banner/help text
     WORKSTATION_DOCS = "https://docs.chef.io/workstation/".freeze
-    WORKSTATION_HEADER = "The Chef command line tool for managing your infrastructure from your workstation.".freeze
+    WORKSTATION_HEADER = "The Cinc command line tool for managing your infrastructure from your workstation.".freeze
   end
 end
